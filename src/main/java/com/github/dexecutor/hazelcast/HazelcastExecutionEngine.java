3e21160852e2f232daa6f7554bcd722bd0c971dd
Nadeem Mohammad <coolmind182006@gmail.com>
2016-10-11 14:35:12 +0530

    Added license

--- src/main/java/com/github/dexecutor/hazelcast/HazelcastExecutionEngine.java
+++ src/main/java/com/github/dexecutor/hazelcast/HazelcastExecutionEngine.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.dexecutor.hazelcast;
 
 import static com.github.dexecutor.core.support.Preconditions.checkNotNull;
@@ -18,7 +35,14 @@ import com.github.dexecutor.core.task.Task;
 import com.github.dexecutor.core.task.TaskExecutionException;
 import com.hazelcast.core.ExecutionCallback;
 import com.hazelcast.core.IExecutorService;
-
+/**
+ * Distributed Execution Engine using Hazelcast
+ * 
+ * @author Nadeem Mohammad
+ *
+ * @param <T> Type of Node/Task ID
+ * @param <R> Type of Node/Task result
+ */
 public class HazelcastExecutionEngine<T extends Comparable<T>, R> implements ExecutionEngine<T, R> {
 
 	private static final Logger logger = LoggerFactory.getLogger(HazelcastExecutionEngine.class);
